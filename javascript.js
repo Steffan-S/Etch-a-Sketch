03c0639c952379287f579c0b6e48ed08b6a7fc35
Steffan-S <steffanscholten@hotmail.com>
2023-10-17 16:36:28 +0200

    Added set grid size button
    Added grid size button to html
    Added button functionality through event listener. Prompt the user
    to type new grid size. Check if it's a number between 0-50 (50 limit).
    If number is between 0-50 then trigger delete grid function (with
    stored currentgridsize variable) and creategrid function with new value

--- javascript.js
+++ javascript.js
@@ -1,19 +1,48 @@
-const mainContainer = document.querySelector('.main-container');
+const mainContainer = document.querySelector('#main-container');
 
-const gridSize = 16;
-const gridTotal = gridSize * gridSize;
+let gridSize = 16; // default grid size (on page load)
+createGrid(gridSize);
 
-for (i = 0; i < gridTotal; i++){
-    const div = document.createElement('div');
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = '#16264C';
-        div.style.borderColor = '#0f1b37';
-    });
-    mainContainer.append(div);
-}
+// Grid size button
+const gridSizeButton = document.querySelector('button');
 
-// function changeColor (){
-//     div.style.backgroundColor = 'red';
-// };
+gridSizeButton.addEventListener('click', () => {
+    let currentGridSize = gridSize;
+    console.log(currentGridSize + ' current grid size');
+    gridSize = prompt('Enter grid size');
 
-console.log(gridTotal);
+    if (gridSize != null && gridSize < 50 && !isNaN(gridSize)) {
+        console.log(gridSize + ' I want a new grid with this grid size');
+        deleteGrid(currentGridSize);
+        createGrid(gridSize);
+    } else {
+        console.log(gridSize + ' not a great succes');
+    }
+});
+
+function createGrid(grid){
+    const gridTotal = grid * grid;    
+    console.log(gridTotal + ' creating new grid');
+
+    for (i = 0; i < gridTotal; i++){
+        const div = document.createElement('div');
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = '#16264C';
+            div.style.borderColor = '#0f1b37';
+        });
+        mainContainer.append(div);
+    }
+};
+
+function deleteGrid(currentGridSize){
+    const currentGridTotal = currentGridSize * currentGridSize;    
+    console.log(currentGridTotal +' deleting this many old grid blocks');
+
+    for (i = currentGridTotal; i > 0; i--){
+        console.log(currentGridSize + ' the loop is running');
+        const mainContainer = document.getElementById('main-container');
+        mainContainer.removeChild(mainContainer.lastChild);
+    }
+};
+
+
