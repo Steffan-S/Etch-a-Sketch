0e8b4abba4a5fc5b4a42d197b6be0ae40b308ed8
Steffan-S <steffanscholten@hotmail.com>
2023-10-17 15:45:48 +0200

    Added mouse hover effect (drawing)
    Added a mouseover event listener to all grid blocks to change
    background and border color when mouse over event triggers.

--- javascript.js
+++ javascript.js
@@ -5,7 +5,15 @@ const gridTotal = gridSize * gridSize;
 
 for (i = 0; i < gridTotal; i++){
     const div = document.createElement('div');
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = '#16264C';
+        div.style.borderColor = '#0f1b37';
+    });
     mainContainer.append(div);
 }
+
+// function changeColor (){
+//     div.style.backgroundColor = 'red';
+// };
 
 console.log(gridTotal);
